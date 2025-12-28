c1018678ad5f8f83acbfd972bb9d42476a0b2c8a
MrSerg1 <sergiotejon96@gmail.com>
2025-12-28 13:19:11 -0500

    feat: add slider to portfolio

--- src/pages/Portfolio.jsx
+++ src/pages/Portfolio.jsx
@@ -1,8 +1,12 @@
+import  Slider  from "@/components/Slider";
+
+
 export function Portfolio() {
   return (
-    <div className="container page-container">
+    <main className="container page-container">
       <h1 className="page-title">Portafolio</h1>
-      <div className="portfolio-grid">
+      <Slider />
+      <section className="portfolio-grid">
         {[1, 2, 3, 4, 5, 6].map((i) => (
           <div key={i} className="portfolio-item group">
             <div className="portfolio-overlay">
@@ -10,7 +14,7 @@ export function Portfolio() {
             </div>
           </div>
         ))}
-      </div>
-    </div>
+      </section>
+    </main>
   );
 }
