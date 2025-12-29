7a61f25431200e18e5fe37e71b9eaf1545cb00c3
MrSerg1 <sergiotejon96@gmail.com>
2025-12-29 11:07:58 -0500

    style portfolio page finished

--- src/pages/Portfolio.jsx
+++ src/pages/Portfolio.jsx
@@ -3,18 +3,19 @@ import  Slider  from "@/components/Slider";
 
 export function Portfolio() {
   return (
-    <main className="container page-container">
-      <h1 className="page-title">Portafolio</h1>
+    <>
       <Slider />
-      <section className="portfolio-grid">
-        {[1, 2, 3, 4, 5, 6].map((i) => (
-          <div key={i} className="portfolio-item group">
-            <div className="portfolio-overlay">
-              <span className="project-title">Project {i}</span>
+      <div className="container page-container">
+        <section className="portfolio-grid">
+          {[1, 2, 3, 4, 5, 6, 7, 8].map((i) => (
+            <div key={i} className="portfolio-item group">
+              <div className="portfolio-overlay">
+                <span className="project-title">Project {i}</span>
+              </div>
             </div>
-          </div>
-        ))}
-      </section>
-    </main>
+          ))}
+        </section>
+      </div>
+    </>
   );
 }
